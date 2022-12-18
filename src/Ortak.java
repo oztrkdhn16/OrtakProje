b891cc29a902f62adb3165e4564c46fa46f2caf4
Fatmatrn <ftmtrn3488@gmail.com>
2022-12-18 21:00:41 +0300

    fatma

--- src/Ortak.java
+++ src/Ortak.java
@@ -3,7 +3,7 @@ public class Ortak {
     public static void main(String[] args) {
 
         System.out.println("Merhaba");
-
+        System.out.println("fatma dosyaya bazi seyler ekledi");
     }
 
 }
